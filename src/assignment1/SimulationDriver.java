aaa35ab2e84677dd7a1fbc69ec254da25edbceff
charlesc <charlesc@cs-32626-107008.win.csupomona.edu>
2015-08-12 11:31:09 -0700

    Update

--- src/assignment1/SimulationDriver.java
+++ src/assignment1/SimulationDriver.java
@@ -30,17 +30,26 @@ public class SimulationDriver {
 		IVoteService iVote = new IVoteService();
 		
 		/* 3) Randomly generate a number students and the answers */
-		int randomSize = (int)(new Random().nextInt(30) + 1);
+		int randomSize = (int)(new Random().nextInt(60) + 1);
 		Student student[] = new Student[randomSize];
 		System.out.println(randomSize);
 		
 		/* 4) Submit all the students' answers to iVote Service */
+		int randomStudents = (int)(new Random().nextInt(randomSize));
+		int randomNumber;
+		
 		for (int i = 0; i < student.length; ++i)
 		{
 			student[i] = new Student("" + i);
-			student[i].submitAnswer(mQuestion);
+			
+		}
+			
+		for (int i = 0; i < randomStudents; ++i)
+		{
+			randomNumber = (int)(new Random().nextInt(randomStudents));
+			student[randomNumber].submitAnswer(mQuestion);
 //			student[i].submitAnswer(sQuestion);
-			System.out.println("Student: " + student[i].getId() + "\t|\t Answer: " + student[i].getAnswer());
+			System.out.println("Student: " + student[randomNumber].getId() + "\t|\t Answer: " + student[randomNumber].getAnswer());
 		}
 		
 		/* 5) Call the iVote Service output function to display the result */
